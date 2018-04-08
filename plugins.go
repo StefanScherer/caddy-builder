c09a536aa10e42c585929a5c78420ef7d5aeaf1b
Stefan Scherer <scherer_stefan@icloud.com>
2018-04-08 17:32:00 +0200

    Add dns providers

--- plugins.go
+++ plugins.go
@@ -5,4 +5,9 @@ import (
 	_ "github.com/miekg/caddy-prometheus"
 	// http.ipfilter
 	_ "github.com/pyed/ipfilter"
+	_ "github.com/caddyserver/dnsproviders/cloudflare"
+	_ "github.com/caddyserver/dnsproviders/digitalocean"
+	_ "github.com/caddyserver/dnsproviders/dnsimple"
+	_ "github.com/caddyserver/dnsproviders/godaddy"
+	_ "github.com/caddyserver/dnsproviders/route53"
 )
